3417e2181beedcbad38ae1f16f4fdda1dc97fe61
Astryda Malinowska <astamalinowska@gmail.com>
2022-06-07 13:55:58 +0200

    generated Article model

---
+++ app/models/aricle.rb
@@ -0,0 +1,2 @@
+class Aricle < ApplicationRecord
+end
---
+++ db/migrate/20220607115534_create_aricles.rb
@@ -0,0 +1,10 @@
+class CreateAricles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :aricles do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
