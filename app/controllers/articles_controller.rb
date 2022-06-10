4f1a995f063e438cfa752717055909e1eb3ab0df
Astryda Malinowska <astamalinowska@gmail.com>
2022-06-10 13:05:18 +0200

    Refactored code for using concerns

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,6 +41,7 @@ class ArticlesController < ApplicationController
       end
   end
 
+  #function for destroying article
   def destroy
       @article = Article.find(params[:id])
       @article.destroy
@@ -50,7 +51,7 @@ class ArticlesController < ApplicationController
 
   private
     def article_params
-        params.require(:article).permit(:title, :body)
+        params.require(:article).permit(:title, :body, :status)
     end
 
 end
